dfc431cb106fd0785f6be66eacf5060ec5a018e0
maaz <maazabbas333@gmail.com>
2025-10-04 13:02:07 +0500

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+
+public class Table{
+    Scanner sc = new Scanner(System.in);
+    public void main(String[] args) {
+        System.out.println("Enter a num : ");
+        int num = sc.nextInt();
+        for(int i = 0; i < 10; i++){
+            System.out.println(num + " * " + i + " = " + num * i);
+        }
+    }
+}
 
