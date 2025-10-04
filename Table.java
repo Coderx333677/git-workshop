d1a7f33c06f1695a24ec2a4d36dab817277f0b2b
maaz <maazabbas333@gmail.com>
2025-10-04 13:09:58 +0500

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Table{
-    Scanner sc = new Scanner(System.in);
-    public void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
         System.out.println("Enter a num : ");
         int num = sc.nextInt();
         for(int i = 0; i < 10; i++){
